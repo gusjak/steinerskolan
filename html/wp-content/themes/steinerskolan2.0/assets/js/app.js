f8e48158f5d5d2722181320e8eaae4b40fb334e4
gusjak <70512255+gusjak@users.noreply.github.com>
2021-10-06 20:26:58 +0200

    Navbar finished

--- html/wp-content/themes/steinerskolan2.0/assets/js/app.js
+++ html/wp-content/themes/steinerskolan2.0/assets/js/app.js
@@ -1,5 +1,60 @@
-const button = document.getElementById('button');
+const navMenu = document.querySelector(".nav-menu");
+const openNavMenu = document.querySelector(".open-nav-menu");
+const closeNavMenu = document.querySelector(".close-nav-menu");
+const menuOverlay = document.querySelector(".menu-overlay");
+const mediaSize = 1023;
 
-button.addEventListener('click', () => {
-    alert('JavaScript Works');
-})
+navMenu.addEventListener("click", (e) => {
+    if(e.target.hasAttribute("data-toggle") && 
+        window.innerWidth <= mediaSize){
+        // Prevent default behaviour on parent link in mobile
+        e.preDefault();
+        
+        const menuItemHasChildren = e.target.parentElement;
+        
+        if(menuItemHasChildren.classList.contains("active")){
+            collapseSubMenu();
+        } else {
+            if(navMenu.querySelector(".menu-item-has-children.active")){
+                collapseSubMenu();
+            }
+            // Expand parent menu containing children
+            menuItemHasChildren.classList.add("active");
+            
+            const subMenu = menuItemHasChildren.querySelector(".sub-menu");
+            subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+        }
+    }
+});
+
+const toggleNav = () => {
+    navMenu.classList.toggle("open");
+    menuOverlay.classList.toggle("active");
+}
+
+openNavMenu.addEventListener("click", toggleNav);
+closeNavMenu.addEventListener("click", toggleNav);
+menuOverlay.addEventListener("click", toggleNav);
+
+
+const collapseSubMenu = () => {
+    navMenu.querySelector(".menu-item-has-children.active .sub-menu")
+    .removeAttribute("style");
+    navMenu.querySelector(".menu-item-has-children.active")
+    .classList.remove("active");
+}
+
+function resizeFix() {
+     if(navMenu.classList.contains("open")){
+         toggleNav();
+     }
+     if(navMenu.querySelector(".menu-item-has-children.active")){
+         collapseSubMenu();
+   }
+}
+
+window.addEventListener("resize", () => {
+   if(this.innerWidth > mediaSize){
+       resizeFix();
+   }
+});
--- html/wp-content/themes/steinerskolan2.0/header.php
+++ html/wp-content/themes/steinerskolan2.0/header.php
@@ -13,49 +13,63 @@
 
     <?php $menuItems = get_menu('header-menu'); ?>
 
-    <header>
 
+    <header>
         <?php if (has_custom_logo()) : ?>
-            <div><?php the_custom_logo(); ?></div>
+            <div class="site-logo"><?php the_custom_logo(); ?></div>
         <?php else : ?>
-            <a href="<?= site_url(); ?>"> <?php bloginfo('name'); ?></a>
+            <a class="site-logo-text" href="<?= site_url(); ?>"> <?php bloginfo('name'); ?></a>
         <?php endif; ?>
 
-        <nav class="navbar">
-            <ul class="navbar-nav">
+        <!-- Hamburger -->
+        <div class="open-nav-menu">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M21 18H3V16H21V18ZM21 13H3V11H21V13ZM21 8H3V6H21V8Z" fill="black" />
+            </svg>
+        </div>
+        <div class="menu-overlay"></div>
+        <nav class="nav-menu">
+            <div class="close-nav-menu">
+                <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20 4 4 20m16 0L4 4l16 16Z" stroke="#000" stroke-width="2" stroke-linecap="round" />
+                </svg>
+            </div>
+            <ul class="menu">
                 <?php $currentPageId = $wp_query->queried_object_id; ?>
                 <?php foreach ($menuItems as $menuItem) : ?>
                     <?php if (sizeof($menuItem->children) === 0) : ?>
                         <!-- Single -->
-                        <li class="nav-item">
+                        <li class="menu-item">
                             <a class="nav-link<?= $menuItem->object_id == $currentPageId ? ' active' : '' ?>" href="<?= $menuItem->url; ?>">
                                 <?= $menuItem->title; ?>
                             </a>
                         </li>
                     <?php else : ?>
                         <!-- Parent -->
-                        <div>
-                            <li class="parent-nav-item">
-                                <a class="nav-link<?= $menuItem->object_id == $currentPageId ? ' active' : '' ?>" href="<?= $menuItem->url; ?>">
-                                    <?= $menuItem->title; ?>
-                                </a>
-                            </li>
-                            <ul class="child-navbar-nav">
+                        <li class="menu-item menu-item-has-children">
+                            <a class="nav-link<?= $menuItem->object_id == $currentPageId ? ' active' : '' ?>" data-toggle="sub-menu" href="#">
+                                <?= $menuItem->title; ?>
+                                <svg class="sub-menu-arrow" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M4 9L12 17L20 9" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                            </a>
+                            <ul class="sub-menu">
                                 <?php foreach ($menuItem->children as $childItem) : ?>
                                     <!-- Children -->
-                                    <li class="nav-item">
-                                        <a class="nav-link<?= $childItem->object_id == $currentPageId ? ' active' : '' ?>" href="<?= $childItem->url ?>">
+                                    <li class="menu-item-child">
+                                        <a class="nav-link-child<?= $childItem->object_id == $currentPageId ? ' active' : '' ?>" href="<?= $childItem->url ?>">
                                             <?= $childItem->title; ?>
                                         </a>
                                     </li>
                                 <?php endforeach; ?>
                             </ul>
-                        </div>
+                        </li>
                     <?php endif; ?>
                 <?php endforeach; ?>
                 <button class="apply" id="button">Ansök</button>
             </ul>
         </nav>
+
     </header>
 
     <main>
