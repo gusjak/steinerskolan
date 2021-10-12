3186eb000a67a0966a1831568b75894ce1ac5cd3
gusjak <70512255+gusjak@users.noreply.github.com>
2021-10-12 11:12:48 +0200

    kitchen grid block

---
+++ html/wp-content/plugins/grid-simple/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '4842694bac19be468811d2cf98b7ad49');
